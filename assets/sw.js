f19107878b2806e96843606610740e4a252bc74c
Mateusz Rzempołuch <mateusz_rzempoluch@icloud.com>
2025-11-19 12:33:26 +0100

    Update sw.js

--- assets/sw.js
+++ assets/sw.js
@@ -2,8 +2,8 @@ var cacheName = 'egui-template-pwa';
 var filesToCache = [
   './',
   './index.html',
-  './eframe_template.js',
-  './eframe_template_bg.wasm',
+  './lista_plac_impl.js',
+  './lista_plac_impl_bg.wasm',
 ];
 
 /* Start the service worker and cache all of the app's content */
